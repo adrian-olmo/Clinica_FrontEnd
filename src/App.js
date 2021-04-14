9caf063f22cd05d986985c4b43805e3f31451e7f
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-04-14 21:38:12 +0200

    Listado Citas (Sin Maquetar)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { AppointmentCard } from "./components/appoinmentCard/AppointmentCard";
+import { AppointmentList } from "./container/AppointmentsList/AppointementList";
 
 function App() {
+  /* const userLoggedRole = "client"; // TODO: traer info del padre */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <AppointmentsList role={userLoggedRole}></AppointmentsList> */}
+      {/* <AppointmentCard></AppointmentCard> */}
+      <AppointmentList>
+        <AppointmentCard></AppointmentCard>
+      </AppointmentList>
     </div>
   );
 }
