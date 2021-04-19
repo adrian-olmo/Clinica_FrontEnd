52db3274e62b9695f9db692166f623ecf676f0d5
Israel Sanchez <50373290+IsrraelS@users.noreply.github.com>
2021-04-19 03:41:05 +0200

    Maquetacion de Home y Dashboard

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { AppointmentCard } from "./components/appoinmentCard/AppointmentCard";
 import { Login } from "./container/Form/Login";
 import { Register } from "./container/Form/Register";
 import { AppointmentList } from "./container/AppointmentsList/AppointementList";
-import { Appointment } from "./components/Appoinment/Appointments"
+import { Appointment } from "./components/Appoinment/Appointments";
+import { Home } from "./container/Home"
+import { Dashboard } from "./container/Dashboard";
 
 function App() {
   /* const userLoggedRole = "client"; // TODO: traer info del padre */
   return (
     <div className='App'>
+      <Home></Home>
       {/* <AppointmentsList role={userLoggedRole}></AppointmentsList> */}
       {/* <AppointmentCard></AppointmentCard> */}
       {<Login></Login>}
@@ -18,6 +21,7 @@ function App() {
       <AppointmentList>
         <AppointmentCard></AppointmentCard>
       </AppointmentList>
+      <Dashboard></Dashboard>
     </div>
   );
 }
